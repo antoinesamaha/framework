253fcae989404523277de5cdc6c0107246ae967c
Antoine SAMAHA <antoine.samaha@01barmaja.com>
2017-02-26 22:29:19 +0200

    Initial upload of focVaadinTheme

---
+++ focVaadinTheme/src/com/foc/focVaadinTheme/FocVaadinTheme.java
@@ -0,0 +1,7 @@
+package com.foc.focVaadinTheme;
+
+import com.vaadin.ui.themes.ValoTheme;
+
+public class FocVaadinTheme extends ValoTheme{
+	public static final String THEME_NAME = "focVaadinTheme";
+}
