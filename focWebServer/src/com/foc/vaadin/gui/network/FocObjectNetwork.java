41d6d8111f4b00884cf7b25aebbd0fe2e177690f
Antoine SAMAHA <antoine.samaha@01barmaja.com>
2017-10-03 18:08:34 +0300

    Small css changes

--- focWebServer/src/com/foc/vaadin/gui/network/FocObjectNetwork.java
+++ focWebServer/src/com/foc/vaadin/gui/network/FocObjectNetwork.java
@@ -1,8 +1,13 @@
 package com.foc.vaadin.gui.network;
 
 import org.vaadin.visjs.networkDiagram.Node;
+import org.vaadin.visjs.networkDiagram.Node.NodeDoubleClickListener;
+import org.vaadin.visjs.networkDiagram.event.node.DoubleClickEvent;
 
+import com.foc.Globals;
+import com.foc.IFocEnvironment;
 import com.foc.desc.FocObject;
+import com.vaadin.ui.UI;
 
 public abstract class FocObjectNetwork<FO extends FocObject> {
 
@@ -29,4 +34,16 @@ public abstract class FocObjectNetwork<FO extends FocObject> {
 	public FocNetwork getNetwork(){
 		return network;
 	}
+	
+	/*
+	public void addClickListener(Node node){
+		NodeDoubleClickListener listener = new NodeDoubleClickListener(node) {
+			@Override
+			public void onFired(DoubleClickEvent arg0) {
+				Globals.showNotification("Hello", "Hello", IFocEnvironment.TYPE_HUMANIZED_MESSAGE);
+			}
+		};
+		getNetwork().addNodeDoubleClickListener(listener);
+	}
+	*/
 }
