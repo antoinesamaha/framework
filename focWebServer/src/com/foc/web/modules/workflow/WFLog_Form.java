f14e88da13b3f1983738928c2b0cb02055e9b9c1
batoule <batoul_ae@hotmail.com>
2017-11-27 15:48:23 +0300

    Added Arabic versions for logs and signatures interfaces.

--- focWebServer/src/com/foc/web/modules/workflow/WFLog_Form.java
+++ focWebServer/src/com/foc/web/modules/workflow/WFLog_Form.java
@@ -15,54 +15,50 @@ import com.vaadin.ui.Component;
 
 @SuppressWarnings("serial")
 public class WFLog_Form extends FocXMLLayout {
-  
-  IWorkflow getIWorkflow(){
-    return (IWorkflow) getFocData();
-  }
-  
-  Workflow getWorkflow(){
-    Workflow workflow = null;
-    IWorkflow iWorkflow = getIWorkflow();
-    if(iWorkflow != null){
-      workflow = getIWorkflow().iWorkflow_getWorkflow();
-    }
-    
-    return workflow;
-  }
-  
-  FocList getLogList(){
-    Workflow workflow = getWorkflow();
-    FocList logList = null;
-    if(workflow != null){
-      logList = workflow.getLogList();
-    }
-    return logList;
-  }
-  
-  @Override
-  protected void afterLayoutConstruction() {
-    super.afterLayoutConstruction();
 
-    FVObjectComboBox comp = (FVObjectComboBox) getComponentByName("WF_CURRENT_STAGE");
-    if(comp != null){
-    	boolean editable = false;
-    	FocObject focObject = (FocObject) getFocData();
-    	if(focObject != null){
-    		FocDesc focDesc = focObject.getThisFocDesc();
-    		if(focDesc != null){
-    			editable = focDesc.workflow_IsAllowSignatureStageModification();
-    		}
-    	}
-    	comp.setEnabled(editable);
-    }
-    
-    FocList logList = getLogList();
-    if(logList != null){
-      XMLViewKey    key          = new XMLViewKey(WFLogDesc.WF_LOG_VIEW_KEY, XMLViewKey.TYPE_TABLE, XMLViewKey.CONTEXT_DEFAULT, XMLViewKey.VIEW_DEFAULT);
-      ICentralPanel centralPanel = XMLViewDictionary.getInstance().newCentralPanel(getMainWindow(), key, logList);
-      addComponent((Component) centralPanel);
-      
-//      getMainWindow().changeCentralPanelContent(centralPanel, true);
-    }
-  }
+	IWorkflow getIWorkflow() {
+		return (IWorkflow) getFocData();
+	}
+
+	Workflow getWorkflow() {
+		Workflow workflow = null;
+		IWorkflow iWorkflow = getIWorkflow();
+		if (iWorkflow != null) {
+			workflow = getIWorkflow().iWorkflow_getWorkflow();
+		}
+		return workflow;
+	}
+
+	FocList getLogList() {
+		Workflow workflow = getWorkflow();
+		FocList logList = null;
+		if (workflow != null) {
+			logList = workflow.getLogList();
+		}
+		return logList;
+	}
+
+	@Override
+	protected void afterLayoutConstruction() {
+		super.afterLayoutConstruction();
+		FVObjectComboBox comp = (FVObjectComboBox) getComponentByName("WF_CURRENT_STAGE");
+		if (comp != null) {
+			boolean editable = false;
+			FocObject focObject = (FocObject) getFocData();
+			if (focObject != null) {
+				FocDesc focDesc = focObject.getThisFocDesc();
+				if (focDesc != null) {
+					editable = focDesc.workflow_IsAllowSignatureStageModification();
+				}
+			}
+			comp.setEnabled(editable);
+		}
+		FocList logList = getLogList();
+		if (logList != null) {
+			XMLViewKey key = new XMLViewKey(WFLogDesc.WF_LOG_VIEW_KEY, XMLViewKey.TYPE_TABLE, XMLViewKey.CONTEXT_DEFAULT, XMLViewKey.VIEW_DEFAULT);
+			ICentralPanel centralPanel = XMLViewDictionary.getInstance().newCentralPanel(getMainWindow(), key, logList);
+			addComponent((Component) centralPanel);
+			// getMainWindow().changeCentralPanelContent(centralPanel, true);
+		}
+	}
 }
