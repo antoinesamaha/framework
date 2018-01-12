91ae996a0e968c1776af2775f58848ca969a4204
Antoine SAMAHA <antoine.samaha@01barmaja.com>
2018-01-12 16:56:35 +0200

    Enhancement of Excel files writing

---
+++ focWebServer/src/com/foc/vaadin/gui/tableExports/EXCELSheet.java
@@ -0,0 +1,84 @@
+package com.foc.vaadin.gui.tableExports;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
+import com.foc.Globals;
+import com.foc.util.Utils;
+
+public class EXCELSheet implements Cloneable {
+	private String name = null;
+	private XSSFSheet sheet = null;
+	private int rowCount = 0;
+	private int columnCount = 0;
+	private XSSFRow row = null;
+	private XSSFCell cell = null;
+
+	public EXCELSheet(String name, XSSFSheet sheet) {
+		this.sheet = sheet;
+		this.name = name;
+		rowCount = 0;
+		columnCount = 0;
+		row = null;
+		cell = null;
+	}
+
+	public void dispose() {
+		sheet = null;
+		row = null;
+		cell = null;
+	}
+	
+	@Override
+	protected EXCELSheet clone() throws CloneNotSupportedException {
+		return (EXCELSheet) super.clone();
+	}
+	
+	public void addNewLine() {
+		try{
+			if(this.sheet != null){
+				this.row = this.sheet.createRow(this.rowCount++);
+				this.columnCount = 0;
+			}
+		}catch (Exception localException){
+			localException.printStackTrace();
+		}
+	}
+	
+	public void setRightToLeft(boolean r2l) {
+		if(this.sheet != null){
+			this.sheet.setRightToLeft(r2l);
+		}
+	}
+	
+	public void addCellValue(Object paramObject, int paramInt) {
+		String str = "";
+		try{
+			while (paramInt > 0){
+				str = str + " ";
+				paramInt--;
+			}
+			addCellValue("\"" + str + paramObject + "\"");
+		}catch (Exception localException){
+			Globals.logException(localException);
+		}
+	}
+
+	public void addCellValue(Object paramObject) {
+		try{
+			XSSFCell localXSSFCell = this.row.createCell(this.columnCount++);
+			if((paramObject instanceof String)){
+				if(Utils.isNumeric((String) paramObject)){
+					double d = Double.parseDouble((String) paramObject);
+					localXSSFCell.setCellValue(d);
+				}else{
+					localXSSFCell.setCellValue((String) paramObject);
+				}
+			}
+		}catch (Exception localException){
+			Globals.logException(localException);
+		}
+	}
+
+}
