27db5c55968946557d8a5aaa151ea38f61823fc5
Antoine SAMAHA <antoine.samaha@01barmaja.com>
2018-09-12 16:41:36 +0300

    Allow to create a PrnLayout printing action for BrowserWindowOpener

---
+++ focWebServer/src/com/foc/web/modules/business/PrnLayout_BrowserWindowOpenerStreamResource.java
@@ -0,0 +1,73 @@
+package com.foc.web.modules.business;
+
+import com.foc.business.printing.PrnLayout;
+import com.foc.business.printing.gui.PrintingAction;
+import com.vaadin.server.DownloadStream;
+import com.vaadin.server.StreamResource;
+
+public class PrnLayout_BrowserWindowOpenerStreamResource extends StreamResource {
+
+	private PrintingAction printingAction = null;
+	private PrnLayout      prnLayout      = null;
+	private boolean        wordDoc = false;
+	
+	public PrnLayout_BrowserWindowOpenerStreamResource(PrnLayout prnLayout, PrintingAction printingaction) {
+		this(prnLayout, printingaction, false);
+	}
+	
+	public PrnLayout_BrowserWindowOpenerStreamResource(PrnLayout prnLayout, PrintingAction printingAction, boolean wordDoc) {
+		super(null, "printnig_"+System.currentTimeMillis()+ (wordDoc ? ".docx" : ".pdf"));
+		this.prnLayout = prnLayout;
+		this.printingAction = printingAction;
+		this.wordDoc = wordDoc;
+		setCacheTime(1);
+	}
+	
+	public void dispose(){
+		printingAction = null;
+		prnLayout = null;
+	}
+	
+	protected boolean isWithLogo() {
+		return true;
+	}
+	
+	public PrintingAction getPrintingAction() {
+		return printingAction;
+	}
+	
+	@Override
+	public DownloadStream getStream() {
+		DownloadStream downloadStream = null;
+		if(getPrintingAction() != null && getPrintingAction().getLauncher() != null){
+			if(prnLayout != null){
+				
+//				if(this.prnLayout_Table.getPrintLogoCheckBox() != null){
+//				  boolean withLogo = this.prnLayout_Table.getPrintLogoCheckBox().getValue();
+//				  this.prnLayout_Table.getPrintingAction().getLauncher().setWithLogo(withLogo);
+//				}
+				
+			  getPrintingAction().getLauncher().setWithLogo(isWithLogo());
+				getPrintingAction().setLaunchedAutomatically(false);
+				
+				byte[] bytes = null;
+				if(wordDoc){
+					bytes = getPrintingAction().getLauncher().printWordDocument(prnLayout);
+				}else{
+					bytes = getPrintingAction().getLauncher().web_FillReport(prnLayout, prnLayout.getFileName());
+				}
+				if(bytes != null){
+					if(wordDoc){
+						setMIMEType("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+					}else{
+						setMIMEType("application/pdf");
+					}
+		  		setStreamSource(new FStreamSource(bytes));
+				}
+				downloadStream = super.getStream();
+			}
+		}
+		return downloadStream;
+	}
+	
+}
