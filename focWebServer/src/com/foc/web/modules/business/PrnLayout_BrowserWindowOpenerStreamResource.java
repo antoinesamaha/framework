af0dff16d5efaaf6d10e113817d52578be2c10ea
Antoine SAMAHA <antoine.samaha@01barmaja.com>
2018-09-24 11:29:05 +0300

    Build error correction

--- focWebServer/src/com/foc/web/modules/business/PrnLayout_BrowserWindowOpenerStreamResource.java
+++ focWebServer/src/com/foc/web/modules/business/PrnLayout_BrowserWindowOpenerStreamResource.java
@@ -1,16 +1,25 @@
 package com.foc.web.modules.business;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
+import com.foc.ConfigInfo;
 import com.foc.business.printing.PrnLayout;
 import com.foc.business.printing.gui.PrintingAction;
+import com.foc.util.Utils;
+import com.vaadin.server.ConnectorResource;
 import com.vaadin.server.DownloadStream;
 import com.vaadin.server.StreamResource;
+import com.vaadin.shared.ApplicationConstants;
 
 public class PrnLayout_BrowserWindowOpenerStreamResource extends StreamResource {
 
 	private PrintingAction printingAction = null;
 	private PrnLayout      prnLayout      = null;
 	private boolean        wordDoc        = false;
-	private String         outputFileNameWithoutExtension = null; 
+	private String         outputFileNameWithoutExtension = null;
+	private String         errorMessage   = null;
 	
 	public PrnLayout_BrowserWindowOpenerStreamResource(PrnLayout prnLayout, PrintingAction printingaction) {
 		this(prnLayout, printingaction, false);
@@ -75,6 +84,23 @@ public class PrnLayout_BrowserWindowOpenerStreamResource extends StreamResource 
 				}
 				downloadStream = super.getStream();
 			}
+		} else {
+			StreamSource source = new StreamSource(){
+				@Override
+				public InputStream getStream() {
+					try{
+						return new ByteArrayInputStream(getErrorMessageAsHTML().getBytes("UTF-8"));
+					}catch (UnsupportedEncodingException e){
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					return null;
+				}
+			};
+			// second, create a StreamResource and pass the previous StreamResource:
+			StreamResource resource = new StreamResource(source, "file.html");
+			resource.setMIMEType("text/html; charset=utf-8");
+			downloadStream = ((ConnectorResource) resource).getStream();
 		}
 		return downloadStream;
 	}
@@ -93,6 +119,29 @@ public class PrnLayout_BrowserWindowOpenerStreamResource extends StreamResource 
 
 	public void setWordDoc(boolean wordDoc) {
 		this.wordDoc = wordDoc;
+	}
+
+	public String getErrorMessageAsHTML() {
+		String message = getErrorMessage();
+		if(Utils.isStringEmpty(message)) {
+			if(ConfigInfo.isArabic()) {
+				message = "الطباعة غير ممكنة";
+			}	else {
+				message = "Could not print report";
+			}
+		}
+		String htmlPrefix = "<div style=\"color:blue;font-family:Arial;font-size:36;position:fixed;float:left;top:50%;left: 50%;transform: translate(-50%, -50%);\" >";
+		String htmlSuffix = "</div>";
+		
+		return htmlPrefix + message + htmlSuffix;
+	}
+	
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
 	}
 	
 }
