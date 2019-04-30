a740f7140f2018fbe7ab325a1f3b7d7bf7666632
Antoine SAMAHA <antoine.samaha@01barmaja.com>
2019-04-30 18:35:34 +0300

    Enhance the CLOB Fields creation in Oracle to use the SECUREFILE mode instead of BASEFILE which allows encryption.

--- foc/src/com/foc/desc/field/FStringField.java
+++ foc/src/com/foc/desc/field/FStringField.java
@@ -60,7 +60,9 @@ public class FStringField extends FField {
   public String getCreationString(String name) {
     if (getProvider()== DBManager.PROVIDER_ORACLE){
     	if(isClob()) {
-    		return " \"" + name + "\" CLOB ";
+    		//return " \"" + name + "\" CLOB ";
+    		return " (\"" + name + "\" CLOB) LOB(\""+name+"\") STORE AS SECUREFILE ";
+//    		add(TESTCLOB CLOB) LOB(TESTCLOB) STORE AS SECUREFILE;
     	} else {
     		return " \"" + name + "\" VARCHAR2" + "(" + getSize() + ") ";
     	}
