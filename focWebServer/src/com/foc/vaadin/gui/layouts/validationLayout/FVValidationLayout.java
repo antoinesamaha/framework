df0c9da22602b6cca923b6649d8289e706e69d56
batoule <33721769+batoule@users.noreply.github.com>
2019-10-17 18:45:27 +0300

    Added another section in the footer to display powered by info.

--- focWebServer/src/com/foc/vaadin/gui/layouts/validationLayout/FVValidationLayout.java
+++ focWebServer/src/com/foc/vaadin/gui/layouts/validationLayout/FVValidationLayout.java
@@ -131,6 +131,7 @@ public class FVValidationLayout extends VerticalLayout {//extends HorizontalLayo
 	private ICentralPanel                  centralPanel         = null;
 //	private ChatSlider                    chatSlider           = null;
 	private HorizontalLayout               mainHorizontalLayout = null;
+	private HorizontalLayout               subHorizontalLayout = null;
 //  private FVHorizontalLayout buttonsLayout = null;
 	/*
 	private Button nextContextHelpButton     = null;
@@ -208,7 +209,11 @@ public class FVValidationLayout extends VerticalLayout {//extends HorizontalLayo
   	setSpacing(false);
   	setCaption(null);
   	setWidth("100%");
-  	setHeight("-1px");
+  	if(isNewLook()) {
+  		setHeight("70px");
+  	}else{
+  		setHeight("-1px");
+  	}
   	
   	mainHorizontalLayout = new HorizontalLayout();
   	addComponent(mainHorizontalLayout);
@@ -229,7 +234,11 @@ public class FVValidationLayout extends VerticalLayout {//extends HorizontalLayo
 			mainHorizontalLayout.setWidth("100%");
 			mainHorizontalLayout.addStyleName("foc-footerFullScreenMargin");
 		}
-		mainHorizontalLayout.setHeight("-1px");	
+		if(isNewLook()) {
+			mainHorizontalLayout.setHeight("50px");	
+		}else{
+			mainHorizontalLayout.setHeight("-1px");	
+		}
     
     addStyleName("noPrint");
     addStyleName("foc-validation");
@@ -237,7 +246,22 @@ public class FVValidationLayout extends VerticalLayout {//extends HorizontalLayo
     
   	initButtonsLayout(showBackButton);
     validationListeners = new ArrayList<IValidationListener>();
-    addTransactionToRecentVisited();
+    addTransactionToRecentVisited();    
+    
+    if(isNewLook()) {
+	    subHorizontalLayout = new HorizontalLayout();
+	    addComponent(subHorizontalLayout);
+	  	setComponentAlignment(subHorizontalLayout, Alignment.BOTTOM_CENTER);
+	  	subHorizontalLayout.setWidth("100%");
+	  	subHorizontalLayout.setHeight("20px");  	
+	  	subHorizontalLayout.setMargin(false);
+	  	subHorizontalLayout.setSpacing(true);
+	  	subHorizontalLayout.setStyleName("foc-poweredBySection");
+	  	if (centralPanel != null && !centralPanel.isFullScreen()) {
+	  		subHorizontalLayout.addStyleName("foc-poweredBySectionPortrait");
+	  	}
+    }
+  	
   }
   
   public void dispose(){
@@ -2608,5 +2632,13 @@ public class FVValidationLayout extends VerticalLayout {//extends HorizontalLayo
 		if(stageLayout != null) {
 			stageLayout.adjustButtonsCaption();
 		}
+	}
+
+	public HorizontalLayout getSubHorizontalLayout() {
+		return subHorizontalLayout;
+	}
+
+	public void setSubHorizontalLayout(HorizontalLayout subHorizontalLayout) {
+		this.subHorizontalLayout = subHorizontalLayout;
 	}
 }
