3aa3b84aeab758889cc4dc57bc96ee3c57027aed
Batoul <batoul_ae@hotmail.com>
2022-02-10 15:52:19 +0300

    Re-updated photo album module version number.

--- foc/src/com/foc/business/photoAlbum/PhotoAlbumManagmentModule.java
+++ foc/src/com/foc/business/photoAlbum/PhotoAlbumManagmentModule.java
@@ -22,7 +22,7 @@ import com.foc.desc.FocModule;
 
 public class PhotoAlbumManagmentModule extends FocModule{
 
-	public static final int VERSION_ID = 1003;
+	public static final int VERSION_ID = 1004;
 	
 	public static final String MODULE_NAME = "DocumentManagement";
 
