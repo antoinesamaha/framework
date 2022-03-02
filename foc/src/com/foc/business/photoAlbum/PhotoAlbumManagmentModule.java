4c7ea937176438b63d977d52f0ecdd6739ee04a8
Batoul <batoul_ae@hotmail.com>
2022-03-02 15:33:20 +0300

    Save the file size in the blob 'PHOTO_ALBUM' table.

--- foc/src/com/foc/business/photoAlbum/PhotoAlbumManagmentModule.java
+++ foc/src/com/foc/business/photoAlbum/PhotoAlbumManagmentModule.java
@@ -22,7 +22,7 @@ import com.foc.desc.FocModule;
 
 public class PhotoAlbumManagmentModule extends FocModule{
 
-	public static final int VERSION_ID = 1004;
+	public static final int VERSION_ID = 1005;
 	
 	public static final String MODULE_NAME = "DocumentManagement";
 
